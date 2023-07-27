f501859e21dc3dfad8d7713b3d955afe2e06af55
wangjustin2009 <139458803+wangjustin2009@users.noreply.github.com>
2023-07-27 21:25:21 +0800

    Create t-21.cpp

---
+++ t-21.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int addNum(int a, int b); 
+int main()
+{
+    int n1,n2,area;
+    printf("Enters two numbers:");
+    scanf("%d %d",&n1,&n2);
+    area = addNum(n1, n2);  
+    printf("area = %d",area);
+    return 0;
+}
+int addNum(int a, int b){ 
+    int result;
+    result = 60*a+b;
+    return result;                     
+}
