1ff71758e966b0116feccbdc5158ac540167b84c
wangjustin2009 <139458803+wangjustin2009@users.noreply.github.com>
2023-07-27 18:55:46 +0800

    Create z-15.cpp

---
+++ z-15.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int n,j=1;
+    scanf("%d",&n);
+    for(int s=1; s<=n; s=s+1){
+        for(int i=1; i<=s; i=i+1){
+            printf("%d",s);
+        }
+        printf("\n");
+        
+    }
+    return 0;
+}
