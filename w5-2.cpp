876b81af19ddce32553c4b8cbeebb27170fe0d01
wangjustin2009 <139458803+wangjustin2009@users.noreply.github.com>
2023-08-03 19:49:20 +0800

    Create w5-2.cpp

---
+++ w5-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int f(int i){
+    if(i <=1){
+        return 1;
+    }else{
+        printf("i = %d \n",i);
+    }
+    return f(i-1);
+}
+/*
+i=5,4,3,2,1
+*/
+int main(){
+    int n = 5;
+    printf("f(%d) = %d \n",n,f(n));
+    return 0;
+}
